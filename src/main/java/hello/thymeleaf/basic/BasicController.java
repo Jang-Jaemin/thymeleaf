a125077cf76f69dfebca5e8608b2c631a0f230a5
admin <wkwovlf12@naver.com>
2022-07-27 16:50:49 +0900

    22.7.27

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,8 +1,16 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -18,5 +26,38 @@ public class BasicController {
         public String textUnescaped(Model model){
             model.addAtrribute("data","Hello <b> Spring! </b>");
             return "basic/text-unescaped";
+    }
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 10);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA",userA);
+        map.put("userB",userB);
+
+        model.addAllAttributes("user",userA);
+        model.addAllAttributes("users", list);
+        model.addAllAttributes("userMap",map);
+
+        return "basic/varialbe";
+
+
+    }
+
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username,int age){
+            this.username = username;
+            this.age = age;
+
+        }
     }
 }
